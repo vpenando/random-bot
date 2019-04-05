e308f7ce6ee3157f1944fd6156c610e00fe25b4f
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-05 22:19:25 +0200

    Update ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -2,6 +2,6 @@ from discord import Client, Message
 from .command import command
 
 @command("ping")
-def ping(client: Client, message: Message) -> Message:
+def ping(client: Client, message: Message, args: list) -> Message:
     msg = "pong {0.author.mention}".format(message)
     return client.send_message(message.channel, msg)
