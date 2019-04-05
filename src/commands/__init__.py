a85a994bd1e4e0bdc66da20f85d1a87b7f4865ae
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-05 22:28:24 +0200

    Update __init__.py

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,11 +1,19 @@
-from commands.ping import ping
+from commands.ping import ping, command
+from discord import Client, Message
 
 commands = {}
+
+@command("help")
+def help(client: Client, message: Message, args: list) -> Message:
+    available_commands = "\n".join(map(lambda s: f' - *!{s}*', commands))
+    msg = f"Available commands:\n{available_commands}"
+    return client.send_message(message.channel, msg)
 
 def add_cmd(func):
     name = func.cmd
     commands.update({name: func})
 
+add_cmd(help)
 add_cmd(ping)
 
 # logs each available command
