c9cffe141b7c06a8201a9525a09e117fd5c0c237
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-05 22:52:38 +0200

    Update __init__.py

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -5,8 +5,8 @@ commands = {}
 
 @command("help")
 def help(client: Client, message: Message, args: list) -> Message:
-    available_commands = "\n".join(map(lambda s: f' - *!{s}*', commands))
-    msg = f"Available commands:\n{available_commands}"
+    available_commands = "\n".join(map(lambda s: ' - *!%s*' % s, commands))
+    msg = "Available commands:\n%s" % available_commands
     return client.send_message(message.channel, msg)
 
 def add_cmd(func):
@@ -18,4 +18,4 @@ add_cmd(ping)
 
 # logs each available command
 for cmd in commands:
-    print(f"Command '{cmd}' added")
+    print("Command '' added" % cmd)
