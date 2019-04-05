2fe01f1c926ab0740805b05fa1b686a7b45c7987
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-05 21:48:55 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -4,17 +4,16 @@ from commands import commands
 
 client = Client()
 
-
 def error(message: Message) -> Message:
     err_msg = f"Error: invalid command '{message.content}''"
     return client.send_message(message.channel, err_msg)
-
 
 @client.event
 async def on_message(message: Message) -> None:
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+    # we only accept commands starting with '!'
     if message.content[0] != '!':
         return
     msg = message.content.split(' ')[0][1:]
@@ -22,7 +21,6 @@ async def on_message(message: Message) -> None:
         await commands[msg](client, message)
     else:
         await error(message)
-
 
 token = sys.argv[1]
 client.run(token)
