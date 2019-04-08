b0dcb6bb785abfef654231646799341817c8ebbc
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-08 13:55:57 +0200

    Update command.py

--- src/commands/command.py
+++ src/commands/command.py
@@ -2,10 +2,10 @@ from discord import Client, Message
 from functools import wraps
 
 def command(cmd):
-    def logging_decorator(func):
+    def cmd_function_builder(func):
         @wraps(func)
         def wrapped_function(client: Client, message: Message, args:list):
             return func(client, message, args)
         wrapped_function.cmd = cmd
         return wrapped_function
-    return logging_decorator
+    return cmd_function_builder
