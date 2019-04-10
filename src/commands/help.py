56ba22ac20135c9aa6a65c779987449ef1e59920
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-10 12:11:29 +0200

    Update help.py

--- src/commands/help.py
+++ src/commands/help.py
@@ -2,7 +2,7 @@
 from discord import Client, Message
 from .core import command, commands
 
-@command("help")
+@command
 def help(client: Client, message: Message, args: list) -> Message:
     available_commands = "\n".join(map(lambda s: " - *!%s*" % s, commands))
     msg = "Available commands:\n%s" % available_commands
