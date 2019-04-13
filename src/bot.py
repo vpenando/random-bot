918b67985c7603ae4e7c41c6db214cacba25c2f2
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-13 23:35:15 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@
 import datetime
 import sys
 from discord import Client, Message
-from commands import commands
+from commands import command, commands
 
 LOG_FILE = "bot.log"
 client = Client()
@@ -16,6 +16,23 @@ def log_call(user: str, cmd: str) -> None:
     datestr = str(datetime.datetime.now()).split(".")[0]                         
     with open(LOG_FILE, "a") as f:                                               
         f.write("[%s] %s: %s\n" % (datestr, user, cmd)) 
+
+@command
+def history(client: Client, message: Message, args: list) -> Message:
+    history_size = 10
+    if len(args) > 0:
+        try:
+            history_size = int(args[0])
+        except ValueError:
+            client.send_message(message.channel,
+                                "Invalid value '%s" % args[0])
+    lines = []
+    with open(LOG_FILE) as f:
+        line = f.readline()
+        lines.append(line)
+    last_lines = lines[-history_size:]
+    msg = "\n".join(last_lines)
+    return client.send_message(message.channel, msg)
 
 @client.event
 async def on_message(message: Message) -> None:
