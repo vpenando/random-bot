b0fc765aee8a00a9e51c89c72deec0f98aea52a7
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-27 00:59:41 +0200

    Update help.py

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,9 +1,12 @@
 # coding: utf-8
+import datetime
 from discord import Client, Message
 from .core import command, commands
 
 @command
 def help(client: Client, message: Message, args: list) -> Message:
+    now = datetime.datetime.now()
+    print('[{0}] Command HELP called by {1.author.name}'.format(now, message))
     available_commands = "\n".join(map(lambda s: " - *!%s*" % s, commands))
     msg = "Available commands:\n%s" % available_commands
     return client.send_message(message.channel, msg)
