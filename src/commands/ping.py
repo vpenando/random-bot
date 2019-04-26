b2606cc24a4c853644039c5082de0e736a32fb2d
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-27 00:52:53 +0200

    Update ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -1,9 +1,11 @@
 # coding: utf-8
+import datetime
 from discord import Client, Message
 from .core import command
 
 @command
 def ping(client: Client, message: Message, args: list) -> Message:
-    print('Command PING called by {0.author.name}'.format(message))
+    now = datetime.datetime.now()
+    print('[{}] Command PING called by {1.author.name}'.format(now, message))
     msg = "pong {0.author.mention}".format(message)
     return client.send_message(message.channel, msg)
