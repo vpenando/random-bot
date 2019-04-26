87360f9dd429681f31f02f0818e0ba9b5a5a0741
vpenando <5292373+vpenando@users.noreply.github.com>
2019-04-27 00:53:39 +0200

    Update ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -6,6 +6,6 @@ from .core import command
 @command
 def ping(client: Client, message: Message, args: list) -> Message:
     now = datetime.datetime.now()
-    print('[{}] Command PING called by {1.author.name}'.format(now, message))
+    print('[{0}] Command PING called by {1.author.name}'.format(now, message))
     msg = "pong {0.author.mention}".format(message)
     return client.send_message(message.channel, msg)
