00a28d0fbdc07c4724ee1ccf00aa18620298be34
vpenando <5292373+vpenando@users.noreply.github.com>
2019-05-16 13:58:23 +0200

    Update log.py

--- src/commands/log.py
+++ src/commands/log.py
@@ -5,6 +5,15 @@ from .core import command
 
 LOG_FILE_PATH = os.getenv('HOME') + '/discord-bot.log'
 
+
+def write_to_log_file(msg: str) -> None
+    try:
+        with open(LOG_FILE_PATH, 'a+') as f:
+            f.write('%s\n' % msg)
+    except:
+        pass
+
+
 @command
 def log(client: Client, message: Message, args: list) -> Message:
   # not implemented yet
