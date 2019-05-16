28fbeafd36cf7162119bf9c916a73acf5613be49
vpenando <5292373+vpenando@users.noreply.github.com>
2019-05-16 14:12:11 +0200

    Update help.py

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,13 +1,11 @@
 # coding: utf-8
-import datetime
 from discord import Client, Message
 from .core import command, commands
 from .log import write_to_log_file
 
 @command
 def help(client: Client, message: Message, args: list) -> Message:
-    now = datetime.datetime.now()
-    write_to_log_file('[{0}] Command HELP called by {1.author.name}'.format(now, message))
+    write_to_log_file('Command HELP called by {1.author.name}'.format(message))
     available_commands = "\n".join(map(lambda s: " - *!%s*" % s, commands))
     msg = "Available commands:\n%s" % available_commands
     return client.send_message(message.channel, msg)
